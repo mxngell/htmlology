74c83d9fa39f95be281978b250c6e07b90e3b367
mxngell187 <madmalik@inbox.ru>
2024-05-21 22:34:27 +0300

    Add statistic block for students

--- public/scripts/profile-page-script.js
+++ public/scripts/profile-page-script.js
@@ -1,3 +1,21 @@
-(function() {
-    
+MicroModal.init({
+    openTrigger: 'data-custom-open',
+    closeTrigger: 'data-custom-close',
+    openClass: 'is-open',
+    awaitOpenAnimation: true,
+    awaitCloseAnimation: true
+});
+
+$('.filter-button').on('click', function(){
+    $('.filter-button').removeClass('active')
+    $(this).addClass('active')
+    const score = $(this).text();
+    const statItems = $('.statistic-item');
+    statItems.each((i, item) => {
+        if(score === 'Все' || $(item).find('.statistic-item-score').text().trim() == score) {
+            $(item).show()
+        } else {
+            $(item).hide()
+        }
+    })
 })
