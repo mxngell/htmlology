ff41c8d233971928437e3723f34c9e950cc24c18
mxngell187 <madmalik@inbox.ru>
2024-05-23 15:52:08 +0300

    Code optimization

--- public/scripts/authorization-page-script.js
+++ public/scripts/authorization-page-script.js
@@ -11,38 +11,30 @@ $(function() {
 
     authorization_form.on('submit', function(e) {
         e.preventDefault();
-        if(formValidation(authorization_form) == true) {
+        if(formValidation()) {
             let form_data = $('#authorization-form').serialize();
             $.post('/authorization/', form_data, function (response) {
-                if(response.access == true) {
-                    window.location.replace('/home')
-                } else {
-                    createAlert('danger', response.message)
-                }
+                response.access == true ? window.location.replace('/home') : createAlert('danger', response.message)
             })
         };
     })
 
-    function formValidation(form) {
+    function formValidation() {
         let result = true;
         deleteErrors(all_inputs);
-        all_inputs.each(function() {
-            isEmpty($(this))
+
+        all_inputs.each(function(index, element) {
+            if($(element).val() == "") createError($(element), 'Поле должно быть заполнено')
          })
-        function isEmpty(element){
-            if(element.val() == "") {
-                createError(element, 'Поле должно быть заполнено')
-            }
-        }
+
         function createError(element, text) {
             const current_element = element.parent()
-            if(current_element.hasClass('input-no-error')) {
-                current_element.removeClass('input-no-error');
-            }
+            if(current_element.hasClass('input-no-error')) current_element.removeClass('input-no-error')
             current_element.addClass('input-with-error');
             current_element.after(`<div class="input-error-message">${text}</div>`)
             result = false;
         }
+
         function deleteErrors(element) {
             const current_element = element.parent()
             if(current_element.hasClass('input-with-error')) {
@@ -50,6 +42,7 @@ $(function() {
                 current_element.next().remove();
             }
         }
+        
         return result;
     }
 })
--- public/scripts/registration-page-script.js
+++ public/scripts/registration-page-script.js
@@ -4,14 +4,10 @@ $(function() {
     
     registration_form.on('submit', function(e) {
         e.preventDefault();
-        if(formValidation(registration_form) == true) { 
+        if(formValidation()) { 
             let form_data = $('#registration-form').serialize();
             $.post('/registration/', form_data, function (response) {
-                if(response.result == true) {
-                    createAlert('success', 'Регистрация прошла успешно')
-                } else {
-                    createAlert('warning', response.message)
-                }
+                response.result == true ? createAlert('success', 'Регистрация прошла успешно') : createAlert('warning', response.message)
             })
         }
     })
@@ -28,9 +24,8 @@ $(function() {
         }
     })
 
-    function formValidation(form, e) {
+    function formValidation() {
         let result = true;
-
         let all_inputs = $('.registration-block-body-inputs input')
         let email = $('input[name="email"]');
         let password = $('input[name="password"]');
@@ -40,9 +35,9 @@ $(function() {
 
         deleteErrors();
 
-        all_inputs.each(function() {
-           isEmpty($(this))
-        })
+        all_inputs.each(function(index, element) {
+            $(element).val() == "" ? createError($(element), 'Поле должно быть заполнено') : successInput($(element))
+         })
         
         if(!email.parent().hasClass('input-with-error')){
             if(!email.val().trim().match(/^\w+@[a-zA-Z_]+?\.[a-zA-Z]{2,3}$/gm)) {
@@ -53,47 +48,28 @@ $(function() {
         if(!password.parent().hasClass('input-with-error')){
             if(password.val().length < 8 || password.val().length > 12){
                 createError(password, 'Пароль должен содержать от 8 до 12 символов');
-            } else {
-                if(!password.val().match(/^(?=.*?[a-z])(?=.*?[0-9])(?=.*?\W)(?!.*\s)(?!.*[а-яА-Я]).{8,12}$/gm)){
-                    createError(password, 'Пароль должен содержать латиницу, хотя бы одну цифру и спецсимвол без пробелов');
-                }
+            } else if (!password.val().match(/^(?=.*?[a-z])(?=.*?[0-9])(?=.*?\W)(?!.*\s)(?!.*[а-яА-Я]).{8,12}$/gm)) {
+                createError(password, 'Пароль должен содержать латиницу, хотя бы одну цифру и спецсимвол без пробелов');
             }
         }
 
         let reg_exp = /^[А-Яа-я]{2,20}$/gm;
 
-        if(!name.parent().hasClass('input-with-error')){
-            if(!name.val().trim().match(reg_exp)) {
-                createError(name, 'Введите правильное имя на кириллице. Например: Василий');
-            }
+        if(!name.parent().hasClass('input-with-error') && !name.val().trim().match(reg_exp)){
+            createError(name, 'Введите правильное имя на кириллице. Например: Василий');
         }
 
-        if(!surname.parent().hasClass('input-with-error')){
-            if(!surname.val().trim().match(reg_exp)) {
-                createError(surname, 'Введите правильную фамилию на кириллице. Например: Смирнов');
-            }
+        if(!surname.parent().hasClass('input-with-error') && !surname.val().trim().match(reg_exp)){
+            createError(surname, 'Введите правильную фамилию на кириллице. Например: Смирнов');
         }
 
-        if(!middle_name.parent().hasClass('input-with-error')){
-            if(!middle_name.val().trim().match(reg_exp)) {
-                createError(middle_name, 'Введите правильное отчество на кириллице. Например: Михайлович');
-            }
-        }
-
-
-        function isEmpty(element){
-            if(element.val() == "") {
-                createError(element, 'Поле должно быть заполнено')
-             } else {
-                successInput(element);
-            }
+        if(!middle_name.parent().hasClass('input-with-error') && !middle_name.val().trim().match(reg_exp)){
+            createError(middle_name, 'Введите правильное отчество на кириллице. Например: Михайлович');
         }
 
         function createError(element, text) {
             const current_element = element.parent()
-            if(current_element.hasClass('input-no-error')) {
-                current_element.removeClass('input-no-error');
-            }
+            if(current_element.hasClass('input-no-error')) current_element.removeClass('input-no-error')
             current_element.addClass('input-with-error');
             current_element.after(`<div class="input-error-message">${text}</div>`)
             result = false;
