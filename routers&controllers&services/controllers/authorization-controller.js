ddb3ac7e23ed808b3f0f2e09ad5afa2bbac3a62a
mxngell187 <madmalik@inbox.ru>
2024-05-28 17:31:08 +0300

    Optimize auth and reg handlers & delete unnecessary services

--- routers&controllers&services/controllers/authorization-controller.js
+++ routers&controllers&services/controllers/authorization-controller.js
@@ -1,4 +1,6 @@
-const authorizationService = require('../services/authorization-service');
+const { checkUser } = require('../services/users-service')
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 exports.getAuthPage = (request, response) => {
     if(request.session.token) {
@@ -10,9 +12,24 @@ exports.getAuthPage = (request, response) => {
 
 exports.auth = async (request, response) => {
     try {
-        const result = await authorizationService.authUser(request.body)
-        if(result.access == true) request.session.token = result.token
-        response.status(200).json(result)
+        const user = await checkUser(request.body.email)
+        if(!user || !bcrypt.compareSync(request.body.password, user.password)) {
+            return response.status(200).json({
+                access: false, 
+                message: 'Неверные данные от профиля'
+            })
+        }
+
+        const accessToken = jwt.sign({
+            id: user.user_id,
+            role: user.role
+        }, process.env.SECRET_KEY, {expiresIn: '1h'})
+
+        request.session.token = accessToken
+        response.status(200).json({
+            access: true,
+            token: accessToken
+        })
     } catch (error) {
         console.log('Error message: '.error ,error) 
         response.status(500).render('500')
--- routers&controllers&services/controllers/registration-controller.js
+++ routers&controllers&services/controllers/registration-controller.js
@@ -1,4 +1,7 @@
-const registrationService = require('../services/registration-service')
+const { checkUser, addUser } = require('../services/users-service')
+const bcrypt = require('bcrypt');
+const ShortUniqueId = require('short-unique-id')
+const { randomUUID } = new ShortUniqueId({ length: 7 });
 
 exports.getRegPage = (request, response) => {
     if(request.session.token) {
@@ -10,8 +13,20 @@ exports.getRegPage = (request, response) => {
 
 exports.reg = async (request, response) => {
     try {
-        const result = await registrationService.regNewUser(request.body)
-        response.status(200).json(result)
+        const { email, password, name, surname, middle_name } = request.body
+        const hashedPassword =  bcrypt.hashSync(password, 7)
+
+        const user = await checkUser(email)
+        if(user) return response.status(200).json({
+            result: false, 
+            message: 'Введённая почта уже зарегистрирована'
+        })
+
+        const user_id = randomUUID()
+        await addUser(user_id, name, surname, middle_name, email, hashedPassword)
+        response.status(200).json({
+            result: true
+        })
     } catch (error) {
         console.log('Error message: '.error ,error) 
         response.status(500).render('500')
