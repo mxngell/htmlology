f47ed079986e0216ce7ccc88eb550fa552434281
mxngell187 <madmalik@inbox.ru>
2024-06-01 12:19:12 +0300

    Code refactoring

--- routers&controllers&services/services/themes-service.js
+++ routers&controllers&services/services/themes-service.js
@@ -1,6 +1,4 @@
 const connect = require('../../models/database')
-const ShortUniqueId = require('short-unique-id')
-const { randomUUID } = new ShortUniqueId({ length: 7 });
 
 exports.getAllThemes = async () => {
     const [themes] = await connect.query(`SELECT * FROM Themes ORDER BY title`)
@@ -17,61 +15,29 @@ exports.getAuthorThemes = async (author_id) => {
     return themes
 }
 
-exports.addTheme = async (title, description, theory, task, author) => {
-    try {
-        const [check_title] = await connect.query(`SELECT theme_id FROM Themes WHERE title = ?`, [title])
-        if(!check_title.length) { 
-            const theme_id = randomUUID()
-            await connect.query(`
-                INSERT INTO Themes (theme_id, title, description, theory, task, author) 
-                VALUES (?, ?, ?, ?, ?, ?)`, [theme_id, title, description, theory, task, author])
-            return {
-                result: true
-            }
-        } else {
-            throw new Error('Тема с таким названием уже существует')
-        }
-    } catch(error) {
-        return {
-            result: false,
-            message: error.message,
-        }
-    }
+exports.checkTitle = async (title) => {
+    const [result] = await connect.query(`SELECT theme_id FROM Themes WHERE title = ?`, [title])
+    return result[0]
+}
+
+exports.addTheme = async (theme_id, title, description, theory, task, author) => {
+    const [{affectedRows}] = await connect.query(`
+    INSERT INTO Themes (theme_id, title, description, theory, task, author) 
+    VALUES (?, ?, ?, ?, ?, ?)`, [theme_id, title, description, theory, task, author])
+    return affectedRows
+
 }
 
 exports.updateTheme = async (theme_id, title, description, theory, task) => {
-    try {
-        const [{affectedRows}] = await connect.query(`
-        UPDATE Themes SET title = ?, description = ?, theory = ?, task = ? WHERE theme_id = ?`, [title, description, theory, task, theme_id])
-        if(affectedRows != 0) {
-            return {
-                result: true
-            }
-        } else {
-            throw new Error('Ошибка при обновлении темы')
-        }
-    } catch(error) {
-        return {
-            result: false,
-            message: error.message
-        }
-    }
+    const [{affectedRows}] = await connect.query(`
+    UPDATE Themes SET title = ?, description = ?, theory = ?, task = ? 
+    WHERE theme_id = ?`, [title, description, theory, task, theme_id])
+    return affectedRows
 } 
 
 exports.deleteTheme = async (theme_id) => {
-    try {
-        const [{affectedRows}] = await connect.query(`DELETE FROM Themes WHERE theme_id = ?`, [theme_id])
-        if(affectedRows != 0) {
-            return {
-                result: true
-            }
-        } else {
-            throw new Error('Ошибка при удалении темы')
-        }
-    } catch(error) {
-        return {
-            result: false,
-            message: error.message
-        }
-    }
+    const [{affectedRows}] = await connect.query(`
+    DELETE FROM Themes 
+    WHERE theme_id = ?`, [theme_id])
+    return affectedRows
 }
