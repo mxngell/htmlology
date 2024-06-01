77544ec3ea54aa0d9f0f8413ad7f4729e05e2e47
mxngell187 <madmalik@inbox.ru>
2024-06-01 12:22:09 +0300

    Add routes (GET & POST & DELETE)

--- routers&controllers&services/routers/statistic-router.js
+++ routers&controllers&services/routers/statistic-router.js
@@ -3,6 +3,11 @@ const router = express.Router()
 
 const statisticController = require('../controllers/statistic-controller')
 
+router.route('/')
+    .get(statisticController.getStatPage)
+    .delete(statisticController.deleteStat)
+    .post(statisticController.addStatistic)
+
 router.get('/:id', statisticController.getUserStat)
 
 module.exports = router
