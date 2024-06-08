0213d422f07c750744a90b1b2438e887c1094bb3
mxngell187 <madmalik@inbox.ru>
2024-06-09 01:36:05 +0300

    Change icons

--- public/scripts/alerts.js
+++ public/scripts/alerts.js
@@ -5,23 +5,35 @@ function createAlert(type, text) {
     const alerts = {
         danger: {
             icon: `
-            <svg class="alert-close-button" viewBox="0 0 24 24" fill="rgb(228,228,228)">
-                <path d="M12 22C6.47715 22 2 17.5228 2 12C2 6.47715 6.47715 2 12 2C17.5228 2 22 6.47715 22 12C22 17.5228 17.5228 22 12 22ZM12 20C16.4183 20 20 16.4183 20 12C20 7.58172 16.4183 4 12 4C7.58172 4 4 7.58172 4 12C4 16.4183 7.58172 20 12 20ZM11 15H13V17H11V15ZM11 7H13V13H11V7Z"></path>
+            <svg class="alert-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g id="Iconly/Regular/Light/Danger Circle">
+                <g id="Danger Circle">
+                <path id="Stroke 1" fill-rule="evenodd" clip-rule="evenodd" d="M12.0001 2.75021C17.1081 2.75021 21.2501 6.89121 21.2501 12.0002C21.2501 17.1082 17.1081 21.2502 12.0001 21.2502C6.89112 21.2502 2.75012 17.1082 2.75012 12.0002C2.75012 6.89121 6.89112 2.75021 12.0001 2.75021Z" stroke="#E4E4E4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path id="Stroke 3" d="M11.9952 8.20432V12.6233" stroke="#E4E4E4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path id="Stroke 5" d="M11.995 15.7962H12.005" stroke="#E4E4E4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </g>
+                </g>
             </svg>
             `,
             className: "danger-alert"
         },
         warning: {
             icon: `
-            <svg class="alert-close-button"  viewBox="0 0 24 24" fill="rgb(228,228,228)">
-                <path d="M11.001 10h2v5h-2zM11 16h2v2h-2z"></path><path d="M13.768 4.2C13.42 3.545 12.742 3.138 12 3.138s-1.42.407-1.768 1.063L2.894 18.064a1.986 1.986 0 0 0 .054 1.968A1.984 1.984 0 0 0 4.661 21h14.678c.708 0 1.349-.362 1.714-.968a1.989 1.989 0 0 0 .054-1.968L13.768 4.2zM4.661 19 12 5.137 19.344 19H4.661z"></path>
+            <svg class="alert-icon" viewBox="0 0 24 24" fill="none" xmlns="http://www.w3.org/2000/svg">
+                <g id="Iconly/Regular/Light/Danger Triangle">
+                <g id="Danger Triangle">
+                <path id="Stroke 1" fill-rule="evenodd" clip-rule="evenodd" d="M4.81397 20.4369H19.197C20.779 20.4369 21.772 18.7269 20.986 17.3529L13.8 4.78793C13.009 3.40493 11.015 3.40393 10.223 4.78693L3.02497 17.3519C2.23897 18.7259 3.23097 20.4369 4.81397 20.4369Z" stroke="#E4E4E4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path id="Stroke 3" d="M12.0024 13.4149V10.3149" stroke="#E4E4E4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                <path id="Stroke 2" d="M11.995 16.5001H12.005" stroke="#E4E4E4" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"/>
+                </g>
+                </g>
             </svg>
             `,
             className: "warning-alert"
         },
         success: {
             icon: `
-            <svg class="alert-close-button"  viewBox="0 0 24 24" fill="none">
+            <svg class="alert-icon"  viewBox="0 0 24 24" fill="none">
                 <circle cx="12" cy="12" r="9" stroke="rgb(228,228,228)" stroke-width="2"/>
                 <path d="M8 12L11 15L16 9" stroke="rgb(228,228,228)" stroke-width="2"/>
             </svg>                       
