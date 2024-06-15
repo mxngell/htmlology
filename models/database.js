5abfc851d0fe5cdc4ecf372d24312e2c44917f34
mxngell187 <madmalik@inbox.ru>
2024-06-15 21:21:54 +0300

    Update db connect file

--- models/database.js
+++ models/database.js
@@ -1,10 +1,11 @@
-const mysql = require('mysql2/promise')
+const { createPool } = require('mysql2/promise')
 
-const connect = mysql.createPool({
-    host       : process.env.DB_HOST,
-    user       : process.env.DB_USER,
-    password   : process.env.DB_PASSWORD,
-    database   : process.env.DB_NAME
+const connect = createPool({
+    host       : process.env.MYSQL_HOST,
+    port       : process.env.MYSQL_PORT,
+    user       : process.env.MYSQL_USER,
+    password   : process.env.MYSQL_ROOT_PASSWORD,
+    database   : process.env.MYSQL_DATABASE_NAME,
 })
 
 module.exports = connect
